269f32380676b3d9c5831847781d258816714e7d
Joshua LeBlanc <joshleblanc94@gmail.com>
2016-10-04 14:55:03 -0300

    Initial setup

--- App/Helpers/haml_helper.rb
+++ App/Helpers/haml_helper.rb
@@ -4,5 +4,17 @@ module Site
     def render_page(page, layout=true)
       haml :"pages/#{page}", layout: layout
     end
+
+    def render_partial(page)
+      haml :"partials/#{page}", layout: false
+    end
+
+    def script(file)
+      "<script srf='#{file}'></script>"
+    end
+    
+    def css(file)
+      "<link rel='stylesheet' type='text/css' href='#{file}'"
+    end
   end
 end
